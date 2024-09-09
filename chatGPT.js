612b2484a9770125ef3004c26fb5bdddd98fda10
admin <artem.zankovskiy@infinno.eu>
2024-09-10 01:45:46 +0300

    Comment

--- chatGPT.js
+++ chatGPT.js
@@ -1,4 +1,4 @@
-const chatGPTKey = process.env.CHAT_GPT_KEY
+// const chatGPTKey = "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"
 
 
 async function askChatGPT(question, sourceText, format) {
